80894eb256c09b2f8a7a60a1b309b48d5424c848
mstowsky <dflyerggl@gmail.com>
2026-01-22 23:00:50 +0400

    Refactor done - using only 1 browser window and 1 authorization session for all tests. Some cosmetic changes.

--- conftest.py
+++ conftest.py
@@ -1,10 +1,35 @@
+import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-@pytest.fixture(scope='function')
+stepik_login_url = 'https://stepik.org/catalog?auth=login'
+
+@pytest.fixture(scope='session')
 def browser():
     print('\n--- Open browser ---')
     browser = webdriver.Chrome()
     yield browser
     print("\n--- Close browser ---")
     browser.quit()
+
+@pytest.fixture(scope='session')
+def auth(browser):
+    print('\n--- Authorization ---')
+    browser.get(stepik_login_url)
+
+    # Ждем загрузку формы авторизации
+    WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.ID, 'ember1558')))
+
+    # Заполняем логин и пароль, нажимаем "Войти"
+    browser.find_element(By.ID, 'id_login_email').send_keys(os.getenv('LOGIN'))
+    browser.find_element(By.ID, 'id_login_password').send_keys(os.getenv('PASSWORD'))
+    browser.find_element(By.CSS_SELECTOR, 'button[type=submit]').click()
+
+    # Ожидание и проверка успешной авторизации
+    WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "navbar__profile-img")))
+
+    print('\n--- Authorization is successful ---')
+    return browser
--- section3/test_lesson6_step5_parametrization.py
+++ section3/test_lesson6_step5_parametrization.py
@@ -2,15 +2,13 @@
 https://stepik.org/lesson/237240/step/5
 '''
 
-import os
 import time
 import math
 import pytest
 from dotenv import load_dotenv
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 load_dotenv()
 
 def stepik_answer():
@@ -32,38 +30,25 @@ message_words = []
 def print_final_message():
     """Фикстура, которая выполняется после всех тестов."""
     yield
-    print(*message_words)
+    print('Stepik\'s hidden message is: ', *message_words, sep='')
 
 @pytest.mark.parametrize('page_link', list_of_pages)
-def test_stepik_open_different_pages(browser, page_link):
+def test_stepik_open_different_pages(auth, page_link):
     print(f'Test for page: {page_link}')
+    browser = auth
     browser.get(page_link)
-
-    # Ждем загрузки страницы до появления кнопки авторизации
-    WebDriverWait(browser, 15).until(EC.visibility_of_element_located((By.ID, 'ember499')))
-
-    login_button = browser.find_element(By.ID, 'ember499')
-    login_button.click()
-    time.sleep(0.3)
-    email_input = browser.find_element(By.ID, 'id_login_email')
-    email_input.send_keys(os.getenv('LOGIN'))
-    password_input = browser.find_element(By.ID, 'id_login_password')
-    password_input.send_keys(os.getenv('PASSWORD'))
-    submit_button = browser.find_element(By.CSS_SELECTOR, 'button[type=submit]')
-    submit_button.click()
-
     time.sleep(5)
 
     # Если есть кнопка "Решить снова", то нажимаем, иначе ничего не делаем.
     try:
         again_button = browser.find_element(By.CSS_SELECTOR, '.again-btn')
         again_button.click()
-        time.sleep(3)
+        time.sleep(2)
     except NoSuchElementException:
         pass
 
     answer_input = browser.find_element(By.CSS_SELECTOR, '.textarea')
-    # Без активации поля через JavaScript значение никак не хотелось вводиться.
+    # Без "активации" поля через JavaScript значение никак не хотелось вводиться.
     browser.execute_script("arguments[0].focus();", answer_input)
     answer_input.send_keys(str(stepik_answer()))
 
@@ -74,6 +59,6 @@ def test_stepik_open_different_pages(browser, page_link):
     try:
         assert expected_text == result_text_actual, f'Error. Expected \'{expected_text}\', but got \'{result_text_actual}\'.'
     except AssertionError as e:
-        print(f'Result text is \'{result_text_actual}\'')
+        print(f'Result text is: \'{result_text_actual}\'')
         message_words.append(result_text_actual)
         raise e
